6975dcfdefd4fa3531258fd491488fe003f1eb82
psmdevgit <mis@pothysswarnamahal.com>
2026-03-05 16:56:10 +0530

    initial

---
+++ routes/api.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const router = express.Router();
+const { sql, getConnection } = require("../db");
+const axios = require("axios");
+
+//============================================================================================================
+
+router.post("/login", async (req, res) => {
+   try {
+        const { username, password } = req.body;
+
+        console.log(username,password);
+
+        const pool = await getConnection();
+
+        const query = `SELECT id, username, role, branch FROM pettyusers
+                WHERE username COLLATE SQL_Latin1_General_CP1_CS_AS = @username AND passcode COLLATE SQL_Latin1_General_CP1_CS_AS = @password`
+
+        const result = await pool.request()
+            .input("username", sql.VarChar, username.toUpperCase())
+            .input("password", sql.VarChar, password)
+            .query(query);
+
+
+            console.log(result)
+        if (result.rowsAffected > 0) {
+            res.json({
+                status: "success",
+                data: result.recordset[0]
+            });
+        } else {
+            res.status(401).json({
+                status: "fail",
+                message: "Invalid username or password"
+            });
+        }
+
+    } catch (error) {
+        console.log("LOGIN ERROR:", error);
+
+        res.status(500).json({
+            status: "error",
+            message: "server issue"
+        });
+    }
+});
+
+
+
+//============================================================================================================
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+// ✅ CORS configuration
+// app.use(
+//   cors({
+//     origin: "http://localhost:3001",   // your React app URL
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true
+//   })
+// );
+
+const allowedOrigins = [
+  "http://localhost:3001",
+  "http://192.168.5.62:454"
+];
+
+app.use(cors({
+  origin: function(origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("CORS not allowed"));
+    }
+  }
+}));
+
+app.use(bodyParser.json());
+
+// Routes
+const apiRoutes = require("./routes/api");
+app.use("/api", apiRoutes);
+
+app.get("/", (req, res) => {
+  res.send("API Running 🚀");
+});
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  console.log(`http://localhost:${PORT}`);
+});
